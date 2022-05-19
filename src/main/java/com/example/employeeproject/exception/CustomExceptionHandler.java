ef70f87f16bc3c8b8072986304679408fec24f50
moin21 <mamoinuddin@gmail.com>
2022-05-19 16:13:27 +0530

    Refactored: Custom exception for validation failure

--- src/main/java/com/example/employeeproject/exception/CustomExceptionHandler.java
+++ src/main/java/com/example/employeeproject/exception/CustomExceptionHandler.java
@@ -3,8 +3,13 @@ package com.example.employeeproject.exception;
 import com.example.employeeproject.dto.ResponseDTO;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Controller Advice Class - CustomExceptionHandler
@@ -16,8 +21,16 @@ public class CustomExceptionHandler {
      * @return - ResponseEntity consisting response DTO and Status Code.
      */
     @ExceptionHandler(CustomException.class)
-    public ResponseEntity<ResponseDTO> handleMethodArgumentNotValidException(CustomException exception) {
+    public ResponseEntity<ResponseDTO> handleNonExistingID(CustomException exception) {
         ResponseDTO responseDTO = new ResponseDTO("Exception while parsing Rest request", exception.getMessage());
+        return new ResponseEntity<ResponseDTO>(responseDTO, HttpStatus.BAD_REQUEST);
+    }
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ResponseDTO> handleMethodArgumentNotValidException(MethodArgumentNotValidException exception){
+        List<ObjectError> errorList = exception.getBindingResult().getAllErrors();
+        List<String> errorMessage = errorList.stream().map(objectError -> objectError.getDefaultMessage())
+                .collect(Collectors.toList());
+        ResponseDTO responseDTO = new ResponseDTO("Exception while parsing Rest request",errorMessage);
         return new ResponseEntity<ResponseDTO>(responseDTO, HttpStatus.BAD_REQUEST);
     }
 }
